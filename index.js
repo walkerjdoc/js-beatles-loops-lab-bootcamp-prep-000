af9ff7b0f59f73f3db644c118eeff95d5c68fe9a
Walker Dunn-O'Connor <walkerj.dunn@gmail.com>
2017-12-17 22:28:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function theBeatlesPlay (musicians,instruments)
-  var newArray =[]
+function theBeatlesPlay (musicians,instruments){
+  var newArray = []
     for (var i=0; i < musicians.length; i++) {
       newArray.push(musicians[i] + "plays" + instruments[i]);
     }
